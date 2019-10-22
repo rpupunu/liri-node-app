1802328f2518ccb23e4cf94d8aef38c7d2c7248d
rpupunu <rpupunu@gmail.com>
2019-10-22 12:24:31 -0600

    Added the spotify song search

--- liri.js
+++ liri.js
@@ -3,28 +3,77 @@
 require("dotenv").config();
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
-var keys = require("./keys.js");
+var keys = require("./key.js");
 var moment = require('moment');
 moment().format();
 var spotify = new Spotify(keys.spotify);
 let operation = process.argv[2];
 let input = process.argv[3];
 
+if(operation === 'do-what-it-says') {
+    false.readFile("random.txt", "utf8", function(error, data) {
+
+        if(error) {
+            return console.log(error);
+        }
+
+        console.log(data);
+
+        var dataArr = data.split(",");
+
+        operation = dataArr[0];
+        input = dataArr[1];
+        evalOperation();
+    });
+}
+
+evalOperation();
+
+function evalOperation() {
 // CONCERT THIS //
-
 if(operation === 'concert-this') {
-    axios.get('htts://rest.bandintown.com/artist/' + input + '/events?app_id=codingbootcamp').then(
+    axios.get('https://rest.bandsintown.com/artists/'+input+'/events?app_id=codingbootcamp').then(
         function(response) {
-            let concert = response.data[0];
+            let Data = response.data[0];
 
-            console.log('Venue Name: ' + concert.venue.name);
-            console.log('Venue Location: ' + concert.venue.city + ' '  + concert.venue.region + ' ' + concert.venue.country);
+            // console.log(Data);
 
-            let concertDate = concert.datetime;
+            console.log('Venue Name: ' + Data.venue.name);
+            console.log('Venue Location: ' + Data.venue.city + ' '  + Data.venue.region + ' ' + Data.venue.country);
+
+            let concertDate = Data.datetime;
             concertDate = moment().utc().format('MM/DD/YYYY');
             console.log('Concert Date: ' + concertDate);
+            console.log('Lineup: ' + Data.lineup[0]+ ', ' + Data.lineup[1]+ ', '+ Data.lineup[2]);
         }
-    )};
+)};
+
+// SPOTIFY-THIS-SONG //
+if(operation === 'spotify-this-song') {
+    spotify.search({ type: 'track', query: input, limit: 1}, function(err, data) {
+        if(err) {
+            return console.log('Error occured: ' + err);
+        }
+
+        let Data = data.tracks.items[0];
+        // console.log(Data);
+
+        let artistList = '';
+        for(let i = 0; i < Data.artists.length; i++) {
+            artistList += Data.artists[i].name + ' ';
+        }
+
+        console.log('Artists: ' + artistList);
+        console.log('Song: ' + Data.name);
+        console.log('Album: ' + Data.album.name);
+        console.log('Preview URL: ' + Data.preview_url);
+    })};
+};
+
+
+
+
+
 
     
 
